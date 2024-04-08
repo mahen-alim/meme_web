1a8898bdbf19dc1fe8811657bfa223be95878a0f
mahen-alim <mahennekkers27@gmail.com>
2024-04-08 11:45:01 +0700

    add: tentang kami menu ui

---
+++ about_me.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me | P Gabut</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background-color: lightyellow;
+            font-family: "Tilt Neon", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 400;
+            font-style: normal;
+            font-variation-settings: "XROT" 0, "YROT" 0;
+            align-items: center;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+        }
+
+        .card {
+            background-color: black;
+            padding: 20px;
+            width: max-content;
+            border-radius: 20px;
+            color: white;
+        }
+
+        .back-con {
+            display: flex;
+            flex-direction: row;
+        }
+
+        .col-12 {
+            width: 500px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="back-con">
+        <a href="javascript:history.back()">
+            <img src="img/back3.png" alt="" style="width: 30px; margin-top: 25px; margin-right: 20px;">
+        </a>
+        <h1>Tentang Kami</h1>
+    </div>
+    <div class="row">
+        <div class="card">
+            <div class="col-12">
+                <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto modi dicta similique, doloremque inventore non accusantium nemo. Dolore accusamus ea pariatur minima tenetur nulla praesentium, odio hic quod recusandae nobis.</h3>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
